fd021b48624cfc0939f1b9a250c439e5bdf0e98d
Miguel Villanueva <miguel.villanueva.lobato@gmail.com>
2019-02-24 15:07:09 +0100

    response includes paymentBrand and result

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5858;
 
 app.listen(port, () => {
- console.log("Server running on port 3000");
+ console.log("Server running on port " + port);
 });
 
-app.get("/url", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+// first level keys
+const KEY_PAYMENT_BRAND = 'paymentBrand';
+const KEY_AMOUNT = 'amount';
+const KEY_CURRENCY = 'currency';
+const KEY_UID = 'uid';
+
+// result keys
+const KEY_RESULT_CODE = 'code';
+const KEY_RESULT_DESCRIPTION = 'description';
+
+// result values.
+const VAL_SUCESS_CODE = '000.000.000';
+const VAL_SUCCESS_DESCRIPTION = 'Transaction successfully processed.';
+
+function parseRequest(req, res)
+{
+  req.is('json');
+  var response = {};
+
+  response[KEY_PAYMENT_BRAND] = req.query[KEY_PAYMENT_BRAND];
+  response[KEY_UID] = req.query[KEY_UID];
+
+  setStatus(response);
+
+  response = JSON.stringify(response);
+
+  return response;
+};
+
+function setStatus(response)
+{
+  var result = {};
+
+  result[KEY_RESULT_CODE] = VAL_SUCESS_CODE;
+  result[KEY_RESULT_DESCRIPTION] = VAL_SUCCESS_DESCRIPTION;
+
+  response["result"] = result;
+
+}
+
+app.post("/v1/payments", (req, res, next) => {
+  var response = parseRequest(req, res);
+  
+  res.type('application/json');
+  res.send(response);
 });
